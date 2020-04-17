3b9f72a993ce41bd8fccde18687f1ef0ca3567af
NiceAndFlowy <honestdetermination@gmail.com>
2020-04-17 13:15:50 -0400

    Display (un)selectable provider cards in list

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,36 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Provider } from "./../models/provider";
+import { Component, OnInit } from "@angular/core";
+import { ProvidersService } from "../services/providers.service";
 
 @Component({
-  selector: 'app-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  selector: "app-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["./list.component.css"],
 })
 export class ListComponent implements OnInit {
-
-  public selectedProviders = [];
-  public unselectedProviders = [
-    {
-      id: '1',
-      name: 'John',
-      address: '123 Greenway Blvd',
-      phone: '8991234321'
-    },
-    {
-      id: '2',
-      name: 'Mary',
-      address: '443 Windwhisper Road',
-      phone: '2233211903'
-    },
-    {
-      id: '3',
-      name: 'Jason',
-      address: '9992 Pumpkin Hollow',
-      phone: '4343219384'
-    }
-  ];
-
-  constructor() {}
-
+  constructor(private providerService: ProvidersService) {}
   ngOnInit() {}
-
 }
